6ae597e4d4f3636a3c3d57bf90efc8099eda6881
Felipe Ferreira <felizpeu@MacBook-Pro-de-vitor-2.local>
2023-05-05 19:23:53 -0300

    add mysql

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -73,6 +73,9 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   )
 
   const calendarWeeks = useMemo(() => {
+    if (!blockedDates) {
+      return []
+    }
     const daysInMonthArray = Array.from({
       length: currentDate.daysInMonth(),
     }).map((_, i) => {
